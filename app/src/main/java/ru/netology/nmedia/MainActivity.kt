b0d46014d19cc9d76e1f57f89d6c6ee1709c0cb9
NIK <nicitos31@yandex.ru>
2024-04-28 17:26:13 +0300

    Layout

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -9,12 +9,10 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.netology_main)
+        println(resources.displayMetrics.heightPixels) // 1794
+        println(resources.displayMetrics.widthPixels) // 1080
+        println(resources.displayMetrics.densityDpi) // 420
+        println(resources.displayMetrics.density) // 2.625
     }
 }
